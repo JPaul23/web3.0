b047df90a764140281df3fa7fd68f63a3c66f023
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-10-31 21:38:10 +0200

    solidy setup and transactions development

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,80 @@
+// SPDX-Licence-Identifier: UNLICENCED
+
+pragma solidity ^0.8.0;
+
+//CONTRACT
+contract Transactions {
+    uint256 transactionCount;
+
+    /// Create event
+    /// address is a type
+    /// @param from a variable name
+    /// @param timestamp: when the transfer happened.
+
+    event Transfer(
+        address from,
+        address reciever,
+        uint256 amount,
+        string message,
+        uint256 timestamp,
+        string keyword
+    );
+
+    // Creating a struct
+    /// @param address is a type
+    /// @param sender name of the property
+
+    struct TransferStruct {
+        address sender;
+        address reciever;
+        uint256 amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    // Array of different transactions
+    //transactions is a valiable
+    TransferStruct[] transactions;
+
+    function addToBlockchain(
+        address payable reciever,
+        uint256 amount,
+        string memory message,
+        string memory keyword
+    ) public {
+        transactionCount += 1;
+        //pushing the transactions
+        transactions.push(
+            TransferStruct(
+                msg.sender,
+                reciver,
+                amount,
+                message,
+                block.timestamp,
+                keyword
+            )
+        );
+        //emitting the transfer
+        emit Transfer(
+            msg.sender,
+            reciver,
+            amount,
+            message,
+            block.timestamp,
+            keyword
+        );
+    }
+
+    function getAllTransactions()
+        public
+        view
+        returns (TransferStruct[] memory)
+    {
+        return transactions;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCount;
+    }
+}
---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,26 @@
+const main = async () => {
+
+  // Generate Instances of contracts.
+  const Transactions = await hre.ethers.getContractFactory("Transactions");
+
+  // instance of transactions
+  const transactions = await Transactions.deploy();
+
+  await transactions.deployed();
+
+  console.log(
+    `Transactions ${transactions} deployed to: ${transactions.address}`
+  );
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error('Error at runMain function ===>', error);
+    process.exit(1);
+  }
+}
+
+runMain();
